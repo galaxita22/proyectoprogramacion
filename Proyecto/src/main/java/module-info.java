58bcba73a131b66c40ba5f5d0eb6da68af365f7d
galaxita22 <102005122+galaxita22@users.noreply.github.com>
2022-03-28 00:03:39 -0300

    Add files via upload
    Agregar archivos de código

---
+++ Proyecto/src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module com.example.proyecto {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens com.example.proyecto to javafx.fxml;
+    exports com.example.proyecto;
+}
